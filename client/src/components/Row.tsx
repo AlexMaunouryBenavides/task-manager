5673108dafb2499106de66a1ff5abc51038d5e0f
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-03-20 12:33:21 +0100

    feat:animation rows

--- client/src/components/Row.tsx
+++ client/src/components/Row.tsx
@@ -1,27 +1,46 @@
 interface IRow {
-  title: string;
+   id?: number;
+   title: string;
+   description: string | null;
+   isOpen: boolean;
+   handlClick: () => void;
 }
+import { BsClockHistory } from "react-icons/bs";
+import { TbTargetArrow } from "react-icons/tb";
 
-function Row({ title }: IRow) {
-  return (
-    <div className="flex flex-col md:flex-row gap-10 md:items-center md:align-baseline justify-start bg-cyan-700 p-2 rounded-sm shadow-sm  text-blue-50">
-      <p>01.</p>
-      <p className="md:w-32">{title}</p>
+function Row({ title, description, handlClick }: IRow) {
+   return (
+      <div className="cursor-pointer">
+         <div
+            onClick={handlClick}
+            onKeyUp={handlClick}
+            className="flex flex-col md:flex-row gap-10 bg-cyan-700 p-2 rounded-sm shadow-sm  text-blue-50"
+         >
+            <p>01.</p>
+            <p className="w-48">{title}</p>
+            <div className="flex items-center gap-2">
+               <BsClockHistory />
+               <p>27%</p>
+            </div>
 
-      <div>
-        <img src="" alt="" />
-        <p>27%</p>
+            <div className="flex items-center gap-2">
+               <TbTargetArrow />
+               <p>6 days</p>
+            </div>
+
+            <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
+               Update
+            </div>
+            <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
+               Delete
+            </div>
+         </div>
+         {/* description */}
+         <div className="acc cursor-pointer overflow-hidden px-4 h-0">
+            <p>{description ?? "No description"}</p>
+         </div>
       </div>
-
-      <div>
-        <img src="" alt="" />
-        <p>6 days</p>
-      </div>
-
-      <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">Update</div>
-      <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">Delete</div>
-    </div>
-  );
+   );
 }
 
 export default Row;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,29 +1,115 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { type ChangeEvent, useRef, useState } from "react";
 import { CiSquarePlus } from "react-icons/ci";
 import Header from "../components/Header";
 import Input from "../components/Input";
 import Row from "../components/Row";
+gsap.registerPlugin(useGSAP);
+// fake data
+const projectsData = [
+  {
+    id: 1,
+    title: "Portfolio Website",
+    description: "Personal portfolio built with React",
+  },
+  { id: 2, title: "E-commerce App", description: null },
+  {
+    id: 3,
+    title: "Task Manager",
+    description: "Simple task management API with Node.js",
+  },
+];
 
 function Dashboard() {
+  // gerer le form
+  const [projects, setProjects] = useState(projectsData);
+  const [formInput, setFormInput] = useState({ title: "", description: "" });
+  console.log(projects);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormInput((prev) => ({ ...prev, [name]: value }));
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!formInput.title.trim()) {
+      alert("Must have a title");
+      return;
+    }
+    const newProject = {
+      id: Date.now(),
+      title: formInput.title,
+      description: formInput.description,
+    };
+    setProjects([newProject, ...projects]);
+
+    setFormInput({ title: "", description: "" });
+  }
+  // animation
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const container = useRef(null);
+  useGSAP(
+    () => {
+      const details = gsap.utils.toArray<HTMLElement>(".acc");
+
+      details.forEach((el, i) => {
+        gsap.to(el, {
+          height: openIndex === i ? "auto" : 0,
+          duration: 0.4,
+          ease: "power2.inOut",
+          overwrite: true,
+        });
+      });
+    },
+    { scope: container, dependencies: [openIndex] },
+  );
   return (
     <div>
       <Header title="Dashboard" />
-
       {/* FORM */}
       <div className="flex justify-center">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="flex items-center gap-5">
-            <CiSquarePlus size={65} className="text-cyan-800 cursor-pointer " />
-            <Input placeholder="your task" />
-            <Input placeholder="project deadline" />
+            <button type="submit">
+              <CiSquarePlus
+                size={65}
+                className="text-cyan-800 cursor-pointer"
+              />
+            </button>
+
+            <Input
+              name="title"
+              value={formInput.title}
+              onChange={handleChange}
+              placeholder="your task"
+            />
+            <Input
+              name="description"
+              value={formInput.description}
+              onChange={handleChange}
+              placeholder="description"
+            />
           </div>
         </form>
       </div>
 
       {/* rows container */}
-      <div className="flex md:flex-col flex-wrap gap-2 align-middle  max-w-2xl m-auto">
-        <Row title="Nike" />
-        <Row title="Amazon" />
-        <Row title="Facebook" />
+      <div
+        ref={container}
+        className=" flex md:flex-col flex-wrap gap-2 align-middle  max-w-3xl m-auto"
+      >
+        {projects.map((project, i) => (
+          <Row
+            key={project.id}
+            isOpen={openIndex === i}
+            handlClick={() => setOpenIndex(openIndex === i ? null : i)}
+            title={project.title}
+            description={project.description}
+          />
+        ))}
       </div>
     </div>
   );
