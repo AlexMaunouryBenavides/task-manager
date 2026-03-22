15dc7197036628ebcfc828997deb69347f4d6baf
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-03-22 23:16:11 +0100

    feat:client register

--- client/src/components/Row.tsx
+++ client/src/components/Row.tsx
@@ -1,46 +1,46 @@
 interface IRow {
-   id?: number;
-   title: string;
-   description: string | null;
-   isOpen: boolean;
-   handlClick: () => void;
+  id?: number;
+  title: string;
+  description: string | null;
+  isOpen: boolean;
+  handlClick: () => void;
 }
 import { BsClockHistory } from "react-icons/bs";
 import { TbTargetArrow } from "react-icons/tb";
 
 function Row({ title, description, handlClick }: IRow) {
-   return (
-      <div className="cursor-pointer">
-         <div
-            onClick={handlClick}
-            onKeyUp={handlClick}
-            className="flex flex-col md:flex-row gap-10 bg-cyan-700 p-2 rounded-sm shadow-sm  text-blue-50"
-         >
-            <p>01.</p>
-            <p className="w-48">{title}</p>
-            <div className="flex items-center gap-2">
-               <BsClockHistory />
-               <p>27%</p>
-            </div>
+  return (
+    <div className="cursor-pointer">
+      <div
+        onClick={handlClick}
+        onKeyUp={handlClick}
+        className="flex flex-col md:flex-row gap-10 bg-cyan-700 p-2 rounded-sm shadow-sm  text-blue-50"
+      >
+        <p>01.</p>
+        <p className="w-48">{title}</p>
+        <div className="flex items-center gap-2">
+          <BsClockHistory />
+          <p>27%</p>
+        </div>
 
-            <div className="flex items-center gap-2">
-               <TbTargetArrow />
-               <p>6 days</p>
-            </div>
+        <div className="flex items-center gap-2">
+          <TbTargetArrow />
+          <p>6 days</p>
+        </div>
 
-            <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
-               Update
-            </div>
-            <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
-               Delete
-            </div>
-         </div>
-         {/* description */}
-         <div className="acc cursor-pointer overflow-hidden px-4 h-0">
-            <p>{description ?? "No description"}</p>
-         </div>
+        <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
+          Update
+        </div>
+        <div className="bg-blue-50 rounded-sm text-sm  text-cyan-800 cursor-pointer shadow-lg p-2">
+          Delete
+        </div>
       </div>
-   );
+      {/* description */}
+      <div className="acc cursor-pointer overflow-hidden px-4 h-0">
+        <p>{description ?? "No description"}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Row;
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,37 +1,83 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import register from "../assets/images/register.webp";
 import FormBtn from "../components/FormBtn";
 import Input from "../components/Input";
+import { useState } from "react";
 
 export default function Register() {
-  return (
-    <section className="flex flex-col lg:flex-row items-center justify-between max-h-screen">
-      <img
-        src={register}
-        alt="login warrior img"
-        className="max-h-screen w-auto flex-1 object-cover"
-      />
-      <div className="flex-1 flex flex-col gap-10 items-center justify-between py-5 md:h-75">
-        <div className="max-w-150">
-          <h1 className="font-tm-title text-5xl md:text-[64px] text-center">
-            Take your tasks and work
-          </h1>
-        </div>
+   const navigate = useNavigate();
+   const [name, setName] = useState("");
+   const [lastname, setLastname] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const handleRegister = async () => {
+      const res = await fetch("http://localhost:3310/api/v1/register", {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         credentials: "include",
+         body: JSON.stringify({ name, lastname, email, password }),
+      });
 
-        <div>
-          <Link to={"/"}>
-            <div className="my-2 w-fit bg-cyan-800 text-blue-50 rounded-sm shadow-lg py-1 px-2 text-[12px] cursor-pointer">
-              Login
+      if (!res.ok) {
+         console.log("register failed");
+         return;
+      }
+      navigate("/login");
+   };
+   return (
+      <section className="flex flex-col lg:flex-row items-center justify-between max-h-screen">
+         <img
+            src={register}
+            alt="login warrior img"
+            className="max-h-screen w-auto flex-1 object-cover"
+         />
+         <div className="flex-1 flex flex-col gap-10 items-center justify-between py-5 md:h-75">
+            <div className="max-w-150">
+               <h1 className="font-tm-title text-5xl md:text-[64px] text-center">
+                  Take your tasks and work
+               </h1>
             </div>
-          </Link>
 
-          <form className="flex flex-col gap-5">
-            <Input placeholder="email" />
-            <Input placeholder="password" />
-            <FormBtn>Login</FormBtn>
-          </form>
-        </div>
-      </div>
-    </section>
-  );
+            <div>
+               <Link to={"/"}>
+                  <div className="my-2 w-fit bg-cyan-800 text-blue-50 rounded-sm shadow-lg py-1 px-2 text-[12px] cursor-pointer">
+                     Login
+                  </div>
+               </Link>
+
+               <form
+                  onSubmit={(e) => {
+                     e.preventDefault();
+                     handleRegister();
+                  }}
+                  className="flex flex-col gap-5"
+               >
+                  <Input
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
+                     placeholder="name"
+                  />
+                  <Input
+                     value={lastname}
+                     onChange={(e) => setLastname(e.target.value)}
+                     placeholder="lastname"
+                  />
+                  <Input
+                     value={email}
+                     onChange={(e) => setEmail(e.target.value)}
+                     placeholder="email"
+                     type="email"
+                  />
+                  <Input
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     type="password"
+                     placeholder="password"
+                  />
+                  <FormBtn type="submit">Login</FormBtn>
+               </form>
+            </div>
+         </div>
+      </section>
+   );
 }
