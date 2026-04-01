2b09755c4dbe99c9eacf385fc8bf0e75a2841590
alexandremaunoury <alexandremaunoury@yahoo.fr>
2026-04-01 12:39:11 +0200

    add real unit test user

---
+++ client/src/test/input.test.tsx
@@ -0,0 +1,17 @@
+import { describe, expect, it } from "vitest";
+import Input from "../components/Input";
+import { render, screen } from "@testing-library/react";
+
+describe("Input component", () => {
+  it("should render the input", () => {
+    // creer mon composant dans le dom virtuel
+    // c'est la que j'utilise jsdom
+    // je dis que je créé un element input avec ce placeholder
+    render(<Input placeholder="toto" />);
+    //screen regarde le html fait par jsdom
+    // ensuite je parcours les element pour trouver celui
+    // avec le placeholder "toto"
+    const input = screen.getByPlaceholderText("toto");
+    expect(input).toBeInTheDocument();
+  });
+});
---
+++ server/tests/unit_test_user/unit_usert.test.ts
@@ -0,0 +1,56 @@
+import type { Response, Request, NextFunction } from "express";
+import userRepository from "../../src/modules/models/userRepository";
+import userController from "../../src/modules/controllers/userController";
+
+// test que si le repo envoie des données
+// le controller fait ce que je lui demande
+// donc j'ai besoin du repo,de ma methode a tester dans le controller
+// et le contenu du test
+
+// remplace le repo par un objet qu'on peut espionner
+jest.mock("../../src/modules/models/userRepository");
+
+describe("user controller", () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    req = {};
+    // espionne la fonction .json
+    res = {
+      json: jest.fn(),
+    };
+    // espionne la fonction next
+    next = jest.fn();
+  });
+
+  it("should return users and status succes", async () => {
+    // creation users
+    const fakeUsers = [
+      {
+        id: 1,
+        name: "toto",
+      },
+      {
+        id: 2,
+        name: "toti",
+      },
+    ];
+    // ici je dit a jest
+    // ne va pas en bdd mais utilise fakeUsers
+    (userRepository.read as jest.Mock).mockResolvedValue(fakeUsers);
+
+    // j execute le code du controller
+    await userController.browse(req as Request, res as Response, next);
+
+    // verifie que le controller a appeler l apli UNE SEUL fois
+    expect(userRepository.read).toHaveBeenCalledTimes(1);
+    // on verifie que les donnée envoyer du repo espionné sont celle
+    // de fakeUsers
+    expect(res.json).toHaveBeenCalledWith(fakeUsers);
+    // on verifie de next n'a pas ete call
+    // sinon c'est que je passe a l erreur
+    expect(next).not.toHaveBeenCalled();
+  });
+});
